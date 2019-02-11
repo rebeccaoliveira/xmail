2581be865fb94f3e0ae39ba49ace11dad7e9dd3f
Rebecca Oliveira <rebecca.d.oliveira@gmail.com>
2019-02-11 17:01:55 +0000

    Updates

---
+++ precache-manifest.0ebc5100a6f2f37eeb26d9e915b62a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10ea7e5a60a5240425e0ab46809ce65c",
+    "url": "/xmail/static/media/dolphin.10ea7e5a.jpg"
+  },
+  {
+    "revision": "b1cccf3d14e8995f1d7776ac8990af13",
+    "url": "/xmail/static/media/avatar.b1cccf3d.jpg"
+  },
+  {
+    "revision": "e7cfe7df3c2e34abb4ef",
+    "url": "/xmail/static/js/runtime~main.e7cfe7df.js"
+  },
+  {
+    "revision": "b7761c82256eac9a5041",
+    "url": "/xmail/static/js/main.b7761c82.chunk.js"
+  },
+  {
+    "revision": "b34feaf9e8d985afb30f",
+    "url": "/xmail/static/js/1.b34feaf9.chunk.js"
+  },
+  {
+    "revision": "b7761c82256eac9a5041",
+    "url": "/xmail/static/css/main.69cb12f4.chunk.css"
+  },
+  {
+    "revision": "fb65cb4e08658a1aefb318e0116aa414",
+    "url": "/xmail/index.html"
+  }
+];
